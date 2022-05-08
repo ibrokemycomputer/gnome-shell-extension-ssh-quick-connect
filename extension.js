ace8e55229301559c71160fa6b1e208b4c476243
Nerd <josh@ibrokemy.computer>
2022-05-07 21:33:45 -0700

    WIP: shell prefs

--- extension.js
+++ extension.js
@@ -1,16 +1,17 @@
-'use strict';
-
-const Gio = imports.gi.Gio;
-const GLib = imports.gi.GLib;
-const GObject = imports.gi.GObject;
+// Base requirements
 const St = imports.gi.St;
-const ByteArray = imports.byteArray;
-
+const Gio = imports.gi.Gio;
+// Base requirements pt 2
 const ExtensionUtils = imports.misc.extensionUtils;
-const Util = imports.misc.util;
 const Me = ExtensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
+// For reading files
+const GLib = imports.gi.GLib;
+const ByteArray = imports.byteArray;
+
+
+const GObject = imports.gi.GObject;
 
 const Config = imports.misc.config;
 const SHELL_MAJOR = parseInt(Config.PACKAGE_VERSION.split('.')[0]);
@@ -25,73 +26,53 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
 
     this.createIcon();
 
-    this.createMenu(this.settings);
+    this.createMenu();
   }
 
-  /**
-   * Creates panel icon
-   * 
-   * @returns {void}
-   */
-  createIcon() {
-    const iconUri = `file://${Me.path}/icons/icon.svg`;
-    const iconFile = Gio.File.new_for_uri(iconUri);
-    const gicon = new Gio.FileIcon({ file: iconFile });
-    const icon = new St.Icon({
-      gicon: gicon,
-      style_class: 'system-status-icon'
-    });
-    return this.add_child(icon);
-  }
+  createMenu() {
+    // Get ~/.ssh/config as string
+    // TODO: Need to fix spawn_command_line_sync per review feedback
+    // TODO: Read from list of sources that a user can set in settings,
+    //       explicit request for '/etc/ssh/ssh_config' 
+    const homeSSH = GLib.get_home_dir() + '/.ssh/config';
+    const homeHostString = ByteArray.toString(GLib.file_get_contents(homeSSH)[1]);
+    log('homeHostString', homeHostString);
 
-  /**
-   * Parses the "ssh-source" setting and creates a menu item for each host
-   * 
-   * @param {Gio.Settings} settings The settings object
-   * @returns {void}
-   */
-  createMenu(settings = this.settings) {
-    let hosts = [];
-    
-    let sshPath = settings.get_string('ssh-source');
+    const etcSSH = '/etc/ssh/ssh_config';
+    const etcHostString = ByteArray.toString(GLib.file_get_contents(etcSSH)[1]);
 
-    let paths = sshPath.split(':');
+    // const hostString = homeHostString;
+    log('hostString', hostString);
+    const hostString = homeHostString + '\n' + etcHostString;
 
-    paths.forEach(path => {
-      // Replcae ~ with home directory
-      path = path.replace('~', GLib.get_home_dir());
-      // Get contents of file
-      const fileStr = ByteArray.toString(
-        GLib.file_get_contents(path)[1]
-      );
-      hosts = hosts.concat(this.parseHosts(fileStr));
-    });
+    // Parse string into array of Hosts
+    this.hosts = hostString
+                .split('\n')
+                .join('{{NEWLINE}}')
+                .split('\r')
+                .join('{{NEWLINE}}')
+                .split('{{NEWLINE}}')
+                .map(item => item.trim())
+                .filter(item => item.indexOf('Host ') === 0)
+                .map(item => item = item.split('Host ')[1]);
 
     // Add listeners  
-    return hosts.forEach(item => {
+    this.hosts.forEach(item => {
+      log('item', item);
       this.menu.addAction(item, e => this.sshToItem(item));
     });
   }
 
   /**
-   * 
-   * @param {String} hostString The ssh config file to parse
-   * @returns {Array} An array of hosts
-   */
-  parseHosts(hostString) {
-    return hostString.split('\n').join('{{NEWLINE}}').split('\r').join('{{NEWLINE}}').split('{{NEWLINE}}')
-          .map(item => item.trim())
-          .filter(item => item.indexOf('Host ') === 0)
-          .map(item => item = item.split('Host ')[1]);
-  }
-
-  /**
    * Spawns a subprocess that opens the defualt terminal and runs `ssh ${item}`
    * @param {String} item The Host to ssh into
+   * @param {String} [sshCommand] The command to run on the remote host
+   * 
    * @returns {void}
    */
-  sshToItem(item) {
-    const command = ['x-terminal-emulator', '-e', 'ssh', item];
+  sshToItem(item, cmd = 'x-terminal-emulator -e ssh') {
+    const command = cmd.split(' ');
+    command.push(item);
     try {
       let proc = Gio.Subprocess.new( command, Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE );
       // The callback is a force exit as there is no need for process communication
@@ -101,19 +82,18 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
     }
   }
 
-  getSettings () {
-    let GioSSS = Gio.SettingsSchemaSource;
-    let schemaSource = GioSSS.new_from_directory(
-      Me.dir.get_child("schemas").get_path(),
-      GioSSS.get_default(),
-      false
-    );
-    let schemaObj = schemaSource.lookup(
-      'org.gnome.shell.extensions.ssh-quick-connect.ibrokemy.computer', true);
-    if (!schemaObj) {
-      throw new Error('cannot find schemas');
-    }
-    return new Gio.Settings({ settings_schema : schemaObj });
+  /**
+   * Boilerplate to create an icon from an SVG
+   */
+  createIcon() {
+    const iconUri = `file://${Me.path}/icons/icon.svg`;
+    const iconFile = Gio.File.new_for_uri(iconUri);
+    const gicon = new Gio.FileIcon({ file: iconFile });
+    const icon = new St.Icon({
+      gicon: gicon,
+      style_class: 'system-status-icon'
+    });
+    this.menu.add_child(icon);
   }
 }
 
@@ -121,7 +101,7 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
  * EXTENSION BOILERPLATE
  */
 
-if (SHELL_MAJOR > 39 || SHELL_MINOR > 30) {
+ if (SHELL_MAJOR > 39 || SHELL_MINOR > 30) {
   SSHQuickConnect = GObject.registerClass(
     { GTypeName: 'SSHQuickConnect' },
     SSHQuickConnect
@@ -131,17 +111,19 @@ if (SHELL_MAJOR > 39 || SHELL_MINOR > 30) {
 let indicator = null;
 
 function init() {
-  // log(`initializing ${Me.metadata.name} version ${Me.metadata.version}`);
+  log(`initializing ${Me.metadata.name} version ${Me.metadata.version}`);
 }
 
 function enable() {
-  // log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
+  log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
+
   indicator = new SSHQuickConnect();
   Main.panel.addToStatusArea(`${Me.metadata.name} Indicator`, indicator);
 }
 
 function disable() {
-  // log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
+  log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
+
   if (indicator !== null) {
     indicator.destroy();
     indicator = null;
