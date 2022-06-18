c3f8eaec8e4a725e40324fcebae3f3d2b17ff8e9
Nerd <josh@ibrokemy.computer>
2022-06-18 12:57:15 -0700

    setting page, watch for settings changes, hacky file change monitor

--- extension.js
+++ extension.js
@@ -1,20 +1,23 @@
 'use strict';
-
+/* Base imports pt 1 */
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
 const St = imports.gi.St;
 const ByteArray = imports.byteArray;
-
+/* Base imports pt 2 */
 const ExtensionUtils = imports.misc.extensionUtils;
 const Util = imports.misc.util;
 const Me = ExtensionUtils.getCurrentExtension();
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
-
+/* Part of my hacky bandaid */
+const Mainloop = imports.mainloop;
+/* Other */
 const Config = imports.misc.config;
 const SHELL_MAJOR = parseInt(Config.PACKAGE_VERSION.split('.')[0]);
 const SHELL_MINOR = parseInt(Config.PACKAGE_VERSION.split('.')[1]);
+
 
 let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
 
@@ -26,7 +29,15 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
     this.createIcon();
 
     this.createMenu(this.settings);
+
+    this.settingsUpdateListener();
+
+    // this.fileChangeListener();
+
+    this.fileListenerHack();
+
   }
+
 
   /**
    * Creates panel icon
@@ -44,6 +55,7 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
     return this.add_child(icon);
   }
 
+
   /**
    * Parses the "ssh-source" setting and creates a menu item for each host
    * 
@@ -51,10 +63,10 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
    * @returns {void}
    */
   createMenu(settings = this.settings) {
-    let hosts = [];
-    
-    let sshPath = settings.get_string('ssh-source');
+    this.menu.removeAll();
 
+    let hosts = [];
+    let sshPath = settings.get_string('ssh-source');
     let paths = sshPath.split(':');
 
     paths.forEach(path => {
@@ -71,7 +83,17 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
     return hosts.forEach(item => {
       this.menu.addAction(item, e => this.sshToItem(item));
     });
+
   }
+
+  // Watch sshPath setting for changes and update menu
+  settingsUpdateListener() {
+    this.settings.connect('changed::ssh-source', () => {
+      log('settings changed');
+      this.createMenu();
+    });
+  }
+
 
   /**
    * 
@@ -79,11 +101,17 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
    * @returns {Array} An array of hosts
    */
   parseHosts(hostString) {
-    return hostString.split('\n').join('{{NEWLINE}}').split('\r').join('{{NEWLINE}}').split('{{NEWLINE}}')
+    return hostString
+          .split('\n')
+          .join('{{NEWLINE}}')
+          .split('\r')
+          .join('{{NEWLINE}}')
+          .split('{{NEWLINE}}')
           .map(item => item.trim())
           .filter(item => item.indexOf('Host ') === 0)
           .map(item => item = item.split('Host ')[1]);
   }
+
 
   /**
    * Spawns a subprocess that opens the defualt terminal and runs `ssh ${item}`
@@ -91,7 +119,9 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
    * @returns {void}
    */
   sshToItem(item) {
-    const command = ['x-terminal-emulator', '-e', 'ssh', item];
+    let terminalCommand = this.getTerminalCommand();
+    let command = terminalCommand.split(' ');
+    command.push(item);
     try {
       let proc = Gio.Subprocess.new( command, Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE );
       // The callback is a force exit as there is no need for process communication
@@ -101,6 +131,38 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
     }
   }
 
+  /**
+   * Watch each ssh-source setting for file changes and update menu
+   */
+  // fileChangeListener() {
+  //   let paths = this.settings.get_string('ssh-source').split(':');
+  //   paths.forEach(path => {
+  //     const file = Gio.File.new_for_path(path);
+  //     const fileMonitor = file.monitor(Gio.FileMonitorFlags.WATCH_MOVES, null);
+  //     fileMonitor.connect('changed', (file, otherFile, eventType) => {
+  //       this.createMenu();
+  //     });
+  //   });
+  // }
+
+  /**
+   * Hack time-based loop to monitor file changes. For whatever reason I'm having
+   * issues with the file monitor.
+   * 
+   * @param {Integer} timeout Time in seconds to recheck files for changes
+   */
+  fileListenerHack(timeout = 10) {
+    Mainloop.timeout_add_seconds(timeout, () => {
+      // log('hacky loop');
+      this.createMenu();
+      return this.fileListenerHack();
+    });
+  }
+
+  /**
+   * 
+   * @returns {Object} User settings object
+   */
   getSettings () {
     let GioSSS = Gio.SettingsSchemaSource;
     let schemaSource = GioSSS.new_from_directory(
@@ -114,6 +176,33 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
       throw new Error('cannot find schemas');
     }
     return new Gio.Settings({ settings_schema : schemaObj });
+  }
+
+  /**
+   * Huge shoutout to the VSCode devs for the inspiration here
+   * 
+   * @returns String Terminal command to use
+   * 
+   * @see https://github.com/microsoft/vscode/blob/dce493cb6e36346ef2714e82c42ce14fc461b15c/src/vs/platform/externalTerminal/node/externalTerminalService.ts#L281-L292
+   */
+  getTerminalCommand() {
+    const file = Gio.File.new_for_path('/etc/debian_version');
+    const isDebian = file.query_info('standard::*', 0, null)?.get_size() > 0;
+    const DESKTOP_SESSION = GLib.getenv('DESKTOP_SESSION');
+    const SSH_COMMAND = ' -e ssh';
+    let LINUX_TERMINAL = 'xterm';
+
+    if (isDebian) {
+      LINUX_TERMINAL = 'x-terminal-emulator';
+    } else if (DESKTOP_SESSION === 'gnome' || DESKTOP_SESSION === 'gnome-classic') {
+      LINUX_TERMINAL = 'gnome-terminal';
+    } else if (GLib.getenv('COLORTERM')) {
+      LINUX_TERMINAL = GLib.getenv('COLORTERM');
+    } else if (GLib.getenv('TERM')) {
+      LINUX_TERMINAL = GLib.getenv('TERM');
+    }
+
+    return LINUX_TERMINAL + SSH_COMMAND;
   }
 }
 
@@ -131,17 +220,17 @@ if (SHELL_MAJOR > 39 || SHELL_MINOR > 30) {
 let indicator = null;
 
 function init() {
-  // log(`initializing ${Me.metadata.name} version ${Me.metadata.version}`);
+  log(`initializing ${Me.metadata.name} version ${Me.metadata.version}`);
 }
 
 function enable() {
-  // log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
+  log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
   indicator = new SSHQuickConnect();
   Main.panel.addToStatusArea(`${Me.metadata.name} Indicator`, indicator);
 }
 
 function disable() {
-  // log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
+  log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`);
   if (indicator !== null) {
     indicator.destroy();
     indicator = null;
