3ac77223b1f6faab4223329ee0382e6574e2c059
Josh Martens <jmartens@pint.com>
2023-11-24 13:21:53 -0800

    Gnome 43/44 Support

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /* Base imports pt 1 */
 const Gio = imports.gi.Gio;
 const GLib = imports.gi.GLib;
@@ -15,17 +15,18 @@ const PanelMenu = imports.ui.panelMenu;
 const Mainloop = imports.mainloop;
 /* Other */
 const Config = imports.misc.config;
-const SHELL_MAJOR = parseInt(Config.PACKAGE_VERSION.split('.')[0]);
-const SHELL_MINOR = parseInt(Config.PACKAGE_VERSION.split('.')[1]);
+const SHELL_MAJOR = parseInt(Config.PACKAGE_VERSION.split(".")[0]);
+const SHELL_MINOR = parseInt(Config.PACKAGE_VERSION.split(".")[1]);
 
 let fileTimer;
 
 let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
-
   _init() {
     super._init(0.0, `${Me.metadata.name} Indicator`, false);
 
-    this.settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.ssh-quick-connect.ibrokemy.computer');
+    this.settings = ExtensionUtils.getSettings(
+      "org.gnome.shell.extensions.ssh-quick-connect.ibrokemy.computer"
+    );
 
     this.createIcon();
 
@@ -36,13 +37,11 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
     // this.fileChangeListener();
 
     this.fileListenerHack();
-
   }
-
 
   /**
    * Creates panel icon
-   * 
+   *
    * @returns {void}
    */
   createIcon() {
@@ -51,15 +50,14 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
     const gicon = new Gio.FileIcon({ file: iconFile });
     const icon = new St.Icon({
       gicon: gicon,
-      style_class: 'system-status-icon'
+      style_class: "system-status-icon",
     });
     return this.add_child(icon);
   }
 
-
   /**
    * Parses the "ssh-source" setting and creates a menu item for each host
-   * 
+   *
    * @param {Gio.Settings} settings The settings object
    * @returns {void}
    */
@@ -67,62 +65,60 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
     this.menu.removeAll();
 
     let hosts = [];
-    let sshPath = settings.get_string('ssh-source');
-    let paths = sshPath.split(':');
+    let sshPath = settings.get_string("ssh-source");
+    let paths = sshPath.split(":");
 
-    paths.forEach(path => {
+    paths.forEach((path) => {
       // Replcae ~ with home directory
-      path = path.replace('~', GLib.get_home_dir());
+      path = path.replace("~", GLib.get_home_dir());
       // Get contents of file
-      const fileStr = ByteArray.toString(
-        GLib.file_get_contents(path)[1]
-      );
+      const fileStr = ByteArray.toString(GLib.file_get_contents(path)[1]);
       hosts = hosts.concat(this.parseHosts(fileStr));
     });
 
-    // Add listeners  
-    return hosts.forEach(item => {
-      this.menu.addAction(item, e => this.sshToItem(item));
+    // Add listeners
+    return hosts.forEach((item) => {
+      this.menu.addAction(item, (e) => this.sshToItem(item));
     });
-
   }
 
   // Watch sshPath setting for changes and update menu
   settingsUpdateListener() {
-    this.settings.connect('changed::ssh-source', () => {
+    this.settings.connect("changed::ssh-source", () => {
       // log('settings changed');
       this.createMenu();
     });
   }
 
-
   /**
-   * 
+   *
    * @param {String} hostString The ssh config file to parse
    * @returns {Array} An array of hosts
    */
   parseHosts(hostString) {
     return hostString
-          .split('\n')
-          .join('{{NEWLINE}}')
-          .split('\r')
-          .join('{{NEWLINE}}')
-          .split('{{NEWLINE}}')
-          .map(item => item.trim())
-          .filter(item => item.indexOf('Host ') === 0)
-          .map(item => item = item.split('Host ')[1]);
+      .split("\n")
+      .join("{{NEWLINE}}")
+      .split("\r")
+      .join("{{NEWLINE}}")
+      .split("{{NEWLINE}}")
+      .map((item) => item.trim())
+      .filter((item) => item.indexOf("Host ") === 0)
+      .map((item) => (item = item.split("Host ")[1]));
   }
-
 
   /**
    * Spawns a subprocess that opens indicator
    **/
   sshToItem(item) {
     let terminalCommand = this.getTerminalCommand();
-    let command = terminalCommand.split(' ');
+    let command = terminalCommand.split(" ");
     command.push(item);
     try {
-      let proc = Gio.Subprocess.new( command, Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE );
+      let proc = Gio.Subprocess.new(
+        command,
+        Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+      );
       // The callback is a force exit as there is no need for process communication
       return proc.communicate_utf8_async(null, null, () => proc.force_exit());
     } catch (e) {
@@ -147,7 +143,7 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
   /**
    * Hack time-based loop to monitor file changes. For whatever reason I'm having
    * issues with the file monitor.
-   * 
+   *
    * @param {Integer} timeout Time in seconds to recheck files for changes
    */
   fileListenerHack(timeout = 10) {
@@ -160,41 +156,44 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
 
   /**
    * Huge shoutout to the VSCode devs for the inspiration here
-   * 
+   *
    * @returns String Terminal command to use
-   * 
+   *
    * @see https://github.com/microsoft/vscode/blob/dce493cb6e36346ef2714e82c42ce14fc461b15c/src/vs/platform/externalTerminal/node/externalTerminalService.ts#L281-L292
    */
   getTerminalCommand() {
-    const file = Gio.File.new_for_path('/etc/debian_version');
+    const file = Gio.File.new_for_path("/etc/debian_version");
     let isDebian = 0;
     try {
-      isDebian = file.query_info('standard::*', 0, null)?.get_size() > 0;
-    } catch(e) {
-      isDebian = 0
+      isDebian = file.query_info("standard::*", 0, null)?.get_size() > 0;
+    } catch (e) {
+      isDebian = 0;
     }
-    const DESKTOP_SESSION = GLib.getenv('DESKTOP_SESSION');
-    const SSH_COMMAND = ' -e ssh';
-    let LINUX_TERMINAL = 'xterm';
-    let CUSTOM_SSH = this.settings.get_string('ssh-command');
+    const DESKTOP_SESSION = GLib.getenv("DESKTOP_SESSION");
+    const SSH_COMMAND = " -e ssh";
+    let LINUX_TERMINAL = "xterm";
+    let CUSTOM_SSH = this.settings.get_string("ssh-command");
 
     if (CUSTOM_SSH !== "DEFAULT") {
       return CUSTOM_SSH;
-    } 
-    
+    }
+
     if (isDebian) {
-      LINUX_TERMINAL = 'x-terminal-emulator';
-    } else if (DESKTOP_SESSION === 'gnome' || DESKTOP_SESSION === 'gnome-classic') {
-      LINUX_TERMINAL = 'gnome-terminal';
-    } else if (GLib.getenv('COLORTERM')) {
-      LINUX_TERMINAL = GLib.getenv('COLORTERM');
-    } else if (GLib.getenv('TERM')) {
-      LINUX_TERMINAL = GLib.getenv('TERM');
+      LINUX_TERMINAL = "x-terminal-emulator";
+    } else if (
+      DESKTOP_SESSION === "gnome" ||
+      DESKTOP_SESSION === "gnome-classic"
+    ) {
+      LINUX_TERMINAL = "gnome-terminal";
+    } else if (GLib.getenv("COLORTERM")) {
+      LINUX_TERMINAL = GLib.getenv("COLORTERM");
+    } else if (GLib.getenv("TERM")) {
+      LINUX_TERMINAL = GLib.getenv("TERM");
     }
 
     return LINUX_TERMINAL + SSH_COMMAND;
   }
-}
+};
 
 /**
  * EXTENSION BOILERPLATE
@@ -202,7 +201,7 @@ let SSHQuickConnect = class SSHQuickConnect extends PanelMenu.Button {
 
 if (SHELL_MAJOR > 39 || SHELL_MINOR > 30) {
   SSHQuickConnect = GObject.registerClass(
-    { GTypeName: 'SSHQuickConnect' },
+    { GTypeName: "SSHQuickConnect" },
     SSHQuickConnect
   );
 }
--- prefs.js
+++ prefs.js
@@ -3,14 +3,13 @@ const { Adw, Gio, Gtk } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
-
-function init() {
-}
+function init() {}
 
 function fillPreferencesWindow(window) {
   // Use the same GSettings schema as in `extension.js`
   const settings = ExtensionUtils.getSettings(
-    'org.gnome.shell.extensions.ssh-quick-connect.ibrokemy.computer');
+    "org.gnome.shell.extensions.ssh-quick-connect.ibrokemy.computer"
+  );
 
   // Create a preferences page and group
   const page = new Adw.PreferencesPage();
@@ -18,42 +17,40 @@ function fillPreferencesWindow(window) {
   page.add(group);
 
   // Create a new preferences row
-  const row = new Adw.ActionRow({ 
-    title: 'SSH Config Locations',
-    subtitle: 'Use PATH syntax (aka `:` separated)'
+  const row = new Adw.ActionRow({
+    title: "SSH Config Locations",
+    subtitle: "Use PATH syntax (aka `:` separated)",
   });
   group.add(row);
 
   // Add Gtk text input
-  const SOURCE_KEY = 'ssh-source';
+  const SOURCE_KEY = "ssh-source";
   const input = new Gtk.Entry();
   input.set_text(settings.get_string(SOURCE_KEY));
-  input.connect('changed', (widget) => {
+  input.connect("changed", (widget) => {
     settings.set_string(SOURCE_KEY, widget.get_text());
   });
 
   // Add the input to the row
   row.add_suffix(input);
 
-
   // Create a new preferences row
-  const rowCommand = new Adw.ActionRow({ 
-    title: 'SSH Command',
-    subtitle: 'Don\'t change from DEFAULT unless you know what you are doing!'
+  const rowCommand = new Adw.ActionRow({
+    title: "SSH Command",
+    subtitle: "Don't change from DEFAULT unless you know what you are doing!",
   });
   group.add(rowCommand);
 
   // Add Gtk text input
-  const SOURCE_KEY_COMMAND = 'ssh-command';
+  const SOURCE_KEY_COMMAND = "ssh-command";
   const inputCommand = new Gtk.Entry();
   inputCommand.set_text(settings.get_string(SOURCE_KEY_COMMAND));
-  inputCommand.connect('changed', (widget) => {
+  inputCommand.connect("changed", (widget) => {
     settings.set_string(SOURCE_KEY_COMMAND, widget.get_text());
   });
 
   // Add the input to the row
   rowCommand.add_suffix(inputCommand);
-
 
   // Add our page to the window
   window.add(page);
